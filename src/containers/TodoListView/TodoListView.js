f7305593105818a2f451b9ffb5628aae460fe530
craigmiller160 <craigmiller160@gmail.com>
2018-12-04 18:04:41 -0500

    Further integrated redux into the project

--- src/containers/TodoListView/TodoListView.js
+++ src/containers/TodoListView/TodoListView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TodoList from '../../components/TodoList/TodoList';
 import ButtonPanel from '../../components/shared/ButtonPanel/ButtonPanel';
+import * as todoListActions from '../../store/actions/todo-list.actions';
 
 class TodoListView extends Component {
 
@@ -15,4 +17,16 @@ class TodoListView extends Component {
 
 }
 
-export default TodoListView;
+const mapStateToProps = state => {
+    return {
+        todos: state.todoList.todos
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddTodo: todo => dispatch(todoListActions.addTodo(todo))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoListView);
