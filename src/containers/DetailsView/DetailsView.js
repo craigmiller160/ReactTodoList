c4caf03017fdbdee591a0bf20d304c6e7dd4fed5
craigmiller160 <craigmiller160@gmail.com>
2018-12-05 14:42:29 -0500

    Added delete button to DetailsView

--- src/containers/DetailsView/DetailsView.js
+++ src/containers/DetailsView/DetailsView.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import TodoDetails from '../../components/TodoDetails/TodoDetails';
 import TitleButtonPanel from '../../components/shared/TitleButtonPanel/TitleButtonPanel';
+import Button from '../../components/shared/Button/Button';
+import classes from './DetailsView.css';
 
 class DetailsView extends Component {
 
     render() {
         return (
-            <div>
-                <TitleButtonPanel title="Todo Details" />
+            <div className={classes.DetailsView}>
+                <TitleButtonPanel title="Todo Details">
+                    <Button label="Delete Todo" />
+                </TitleButtonPanel>
                 <TodoDetails />
             </div>
         );
