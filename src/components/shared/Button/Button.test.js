9afc2311e8990424474aebc6ff78b1e5a26330ad
craigmiller160 <craigmiller160@gmail.com>
2018-12-06 10:37:15 -0500

    Made some progress with testing the Button component

--- src/components/shared/Button/Button.test.js
+++ src/components/shared/Button/Button.test.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Button from './Button';
+
+configure({adapter: new Adapter()});
 
 describe('Button component', () => {
-    
+    const BUTTON_LABEL = 'ButtonLabel';
+    const BACKGROUND_COLOR = 'blue';
+    const COLOR = 'red';
+    const BORDER_COLOR = 'gray';
+
+    let props;
+    let mountedButton;
+    const button = () => {
+        if (!mountedButton) {
+            mountedButton = mount(
+                <Button {...props} />
+            );
+        }
+
+        return mountedButton;
+    };
+
+    beforeEach(() => {
+        props = {
+            label: BUTTON_LABEL,
+            backgroundColor: BACKGROUND_COLOR,
+            color: COLOR,
+            borderColor: BORDER_COLOR,
+        };
+
+        //TODO missing the click prop
+
+        mountedButton = undefined;
+    });
+
+    it('renders a button', () => {
+        const buttons = button().find('button');
+        expect(buttons.length).toEqual(1);
+    });
+
+
 });
